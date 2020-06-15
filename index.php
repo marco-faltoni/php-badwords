2c4e971015bf08b9f3128b70842bffa96fd0122a
Marco <marco.faltoni@outlook.it>
2020-06-15 15:35:25 +0200

    prove

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate una "badword" che viene passata in GET con *** (3 simboli di asterico).
+Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso. -->
+
+<?php
+
+$testo = $_GET['lorem ipsum'];
+var_dump($testo);
+
+
+?>
